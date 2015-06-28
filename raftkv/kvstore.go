99d7e99fd18a75374ecaef1e2968db4e025659c7
Wu Lin <554061513@qq.com>
2015-06-28 19:48:40 +0800

    remove raftkv

--- raftkv/kvstore.go
+++
@@ -1,9 +0,0 @@
-package raftkv
-
-// KVstore interface has get and set function to be implemented
-type KVstore interface {
-	Get(key []byte) ([]byte, error)
-	Put(key, val []byte) error
-	Delete(key []byte) error
-	Iter(func(keyBytes []byte) error) error
-}
